8de90d5782d012b349ed2a09e9e431c398a91f74
JY <jyap384@gmail.com>
2024-10-05 13:14:36 +0800

    Initial commit

---
+++ src/main/java/com/example/Ecommerce_user/EcommerceUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ecommerce_user/EcommerceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
